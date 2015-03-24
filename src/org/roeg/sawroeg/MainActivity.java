fc26a5886bd4c6bbfdfc19dd0787b9ccab5a75bb
Libertas <horizonvei@gmail.com>
2015-03-24 22:53:26 +0800

    add enter function

--- src/org/roeg/sawroeg/MainActivity.java
+++ src/org/roeg/sawroeg/MainActivity.java
@@ -2,17 +2,36 @@ package org.roeg.sawroeg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Toast.makeText(MainActivity.this, "Anqcoux Ma Yungh Sawroeg~", Toast.LENGTH_SHORT).show();
+		
+		final EditText text = (EditText) findViewById(R.id.editText1);
+		text.setOnKeyListener(new View.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if(event.getAction() == KeyEvent.ACTION_DOWN)
+					if(keyCode == KeyEvent.KEYCODE_ENTER){
+						String i = text.getText().toString();
+						Toast.makeText(MainActivity.this, i, Toast.LENGTH_SHORT).show();
+						text.setText("");
+						return true;
+					}
+				return false;
+			}
+		});
+		
 	}
 
 	@Override
