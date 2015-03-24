0b7a45ca9d8f8b2ad0ba7496a8a6151f122035e6
Libertas <horizonvei@gmail.com>
2015-03-24 21:43:40 +0800

    add toast

--- src/org/roeg/sawroeg/MainActivity.java
+++ src/org/roeg/sawroeg/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(MainActivity.this, "Anqcoux Ma Yungh Sawroeg~", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
