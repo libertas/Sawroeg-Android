ed5825f7c0311a4fcb6a4a36f45065953275e5b3
Libertas <horizonvei@gmail.com>
2015-04-15 18:19:52 +0800

    add ChanllengeActivity,buggy

--- src/org/roeg/sawroeg/MainActivity.java
+++ src/org/roeg/sawroeg/MainActivity.java
@@ -159,6 +159,11 @@ public class MainActivity extends Activity {
 			startActivity(intent);
 			return true;
 		}
+		if (id == R.id.diujcienq) {
+			Intent intent = new Intent(this, ChallengeActivity.class);
+			startActivity(intent);
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 }
