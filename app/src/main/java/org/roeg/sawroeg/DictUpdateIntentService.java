6c769d229968a074a4ec1b14a8653d37fe3569cb
libertas <horizonvei@gmail.com>
2017-10-31 21:40:05 +0800

    can now check remote db version

--- app/src/main/java/org/roeg/sawroeg/DictUpdateIntentService.java
+++ app/src/main/java/org/roeg/sawroeg/DictUpdateIntentService.java
@@ -3,12 +3,22 @@ package org.roeg.sawroeg;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DictUpdateIntentService extends IntentService {
 
     private static final String ACTION_DictUpdate = "org.roeg.sawroeg.action.DICTUPDATE";
 
-    private static final String EXTRA_DBNAME = "org.roeg.sawroeg.extra.DBNAMETOUPDATE";
+    private static final String EXTRA_DBNAME = "org.roeg.sawroeg.extra.DBVERSIONTOUPDATE";
 
     public DictUpdateIntentService() {
         super("DictUpdateIntentService");
@@ -33,6 +43,51 @@ public class DictUpdateIntentService extends IntentService {
     }
 
     private void handleActionDictUpdate(String dbname) {
-        //throw new UnsupportedOperationException("Not yet implemented");
+        int version_net = 0;
+        int version = 0;
+
+        try {
+            HttpURLConnection connection = null;
+            URL versionUrl = new URL("http://sawroeg.rliber.com/download/db/version.txt");
+            connection=(HttpURLConnection)versionUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream in = connection.getInputStream();
+
+            BufferedReader bufr=new BufferedReader(new InputStreamReader(in));
+            StringBuilder response = new StringBuilder();
+            String line=null;
+            while((line = bufr.readLine())!=null){
+                response.append(line);
+            }
+
+            if(connection != null) {
+                connection.disconnect();
+            }
+
+            version_net = Integer.valueOf(response.toString());
+
+            File f = new File(dbname + ".net");
+            SQLiteDatabase db;
+            if(f.exists()) {
+                db = openOrCreateDatabase(dbname + ".net", MODE_PRIVATE, null);
+            } else {
+                db = openOrCreateDatabase(dbname, MODE_PRIVATE, null);
+            }
+
+            version = DBHelper.getDBVersion(db);
+            db.close();
+        } catch(MalformedURLException e1) {
+            System.out.println(e1);
+        } catch(IOException e2) {
+            System.out.println(e2);
+        } catch(NumberFormatException e3) {
+            System.out.println(e3);
+            System.out.println("Exception on version code");
+        }
+
+        if(version_net > version) {
+            System.out.println("Updating");
+        }
     }
 }
--- app/src/main/java/org/roeg/sawroeg/MainActivity.java
+++ app/src/main/java/org/roeg/sawroeg/MainActivity.java
@@ -67,49 +67,12 @@ public class MainActivity extends AppCompatActivity {
 		itemsArray.notifyDataSetChanged();
 	}
 
-	private void copyFile(InputStream from, OutputStream to) {
-		try {
-			byte[] buffer = new byte[1024];
-			int readBytes = 0;
-			while ((readBytes = from.read(buffer)) != -1)
-				to.write(buffer, 0, readBytes);
-			from.close();
-			to.close();
-		} catch (Exception e1) {
-			throw new Error("Unable to create database");
-		} finally {
-		}
-	}
-
-	private int getDBVersion(SQLiteDatabase db) {
-		Cursor c;
-
-		try {
-			c = db.rawQuery("SELECT value FROM info WHERE key = 'version'", null);
-		} catch(SQLiteException e) {
-			return 0;
-		}
-
-		if(c.moveToNext()) {
-			String version_str = c.getString(c.getColumnIndex("value"));
-			int version;
-			try {
-				version = Integer.valueOf(version_str).intValue();
-			} catch(NumberFormatException e) {
-				version = 0;
-			}
-			return version;
-		} else {
-			return 0;
-		}
-	}
-
 	private void checkDatabase(String dbname) {
 		try {
 			InputStream in = getResources().getAssets().open(dbname);
 			OutputStream out = new FileOutputStream("data/data/org.roeg.sawroeg/databases/"
 					+ dbname);
-			copyFile(in, out);
+			DBHelper.copyFile(in, out);
 		} catch (Exception e1) {
 			throw new Error("Unable to create database");
 		} finally {
