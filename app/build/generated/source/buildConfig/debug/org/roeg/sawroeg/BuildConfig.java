7fd8dfe2c56374f5cb810ca49e99f1bdbe51be22
libertas <horizonvei@gmail.com>
2017-10-31 22:25:41 +0800

    version 3.0

--- app/build/generated/source/buildConfig/debug/org/roeg/sawroeg/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/org/roeg/sawroeg/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.roeg.sawroeg";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 6;
-  public static final String VERSION_NAME = "2.1.1";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "3.0";
 }
