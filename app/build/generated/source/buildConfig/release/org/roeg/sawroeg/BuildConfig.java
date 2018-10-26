907e3114de12d5cc6a1c7cc768e25de43a813b49
libertas <horizonvei@gmail.com>
2018-10-26 15:36:25 +0800

    modify version to 4.0

--- app/build/generated/source/buildConfig/release/org/roeg/sawroeg/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/org/roeg/sawroeg/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.roeg.sawroeg";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 8;
-  public static final String VERSION_NAME = "3.2";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "4.0";
 }
